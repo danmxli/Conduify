9b5613047e37693da5cc33df1bdb084b074b727d
danmxli <danmuxingli@gmail.com>
2023-10-19 13:07:27 -0400

    migrate to fastAPI

---
+++ backend/main.py
@@ -0,0 +1,34 @@
+from typing import Union
+from fastapi import FastAPI
+from dotenv import load_dotenv
+from pymongo.mongo_client import MongoClient
+import os
+from fastapi.encoders import jsonable_encoder
+
+load_dotenv()
+client = MongoClient(os.getenv("MONGODB_URI"))
+db = client['AppData']
+app = FastAPI()
+
+# root route
+@app.get("/")
+def read_root():
+    return {"Root Route": True}
+
+# testing db connection
+@app.get("/connect")
+def connect_db():
+    collection = db["UserInfo"]
+    result = list(collection.find({}))
+    return result
+
+# sign user up
+
+# get user info
+@app.get("/users/{_id}")
+def read_item(_id: str):
+    collection = db['UserInfo']
+    user = collection.find_one({"_id": _id})
+    if not user:
+        return {"message": "user not found"}, 404
+    return user
