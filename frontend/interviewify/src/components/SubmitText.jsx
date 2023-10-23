7084bbb2adb6ff619de34e8e463bc4e4010ca2d1
danmxli <danmuxingli@gmail.com>
2023-10-23 12:29:23 -0400

    duplicate props fix

--- frontend/interviewify/src/components/SubmitText.jsx
+++ frontend/interviewify/src/components/SubmitText.jsx
@@ -5,7 +5,7 @@ const SubmitText = ({ value, onChange, placeholder }) => {
         <form>
 
             <div className="w-full px-4 py-4 rounded-3xl bg-gray-600">
-                <textarea id="comment" rows="4" className="w-full px-0 focus:outline-none text-sm bg-gray-600 focus:ring-0 text-white placeholder-gray-400" placeholder="Write a comment..." required
+                <textarea id="comment" rows="4" className="w-full px-0 focus:outline-none text-sm bg-gray-600 focus:ring-0 text-white placeholder-gray-400" required
                     value={value}
                     onChange={onChange}
                     placeholder={placeholder}
