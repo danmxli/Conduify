c9f8b1167a1438b459ad0a757b567dc709a66653
danmxli <danmuxingli@gmail.com>
2023-10-24 13:52:47 -0400

    cors update and clear sessions

--- frontend/interviewify/src/components/Sidebar.jsx
+++ frontend/interviewify/src/components/Sidebar.jsx
@@ -1,10 +1,39 @@
-import React from 'react';
 import { BsPhoneFlip } from 'react-icons/bs'
 import { FaUserAstronaut } from 'react-icons/fa'
 import { useNavigate } from "react-router-dom";
 
 const Sidebar = ({ open, toggleSidebar, companyData, sessionData }) => {
     const navigate = useNavigate()
+
+    const clearSessions = () => {
+        try {
+            fetch('http://localhost:8001/clear-collections', {
+                method: 'GET',
+                headers: {
+                    'Accept': 'application/json'
+                },
+            })
+                .then(response => {
+                    if (response.ok) {
+                        return response.json();
+                    } else {
+                        console.error('Request failed with status:', response.status);
+                    }
+                })
+                .then(data => {
+                    if (data) {
+                        console.log('Successful response:', data);
+                        window.location.reload();
+                    }
+                })
+                .catch(error => {
+                    console.error('Request failed:', error);
+                });
+        }
+        catch (error) {
+            console.error('Fetch request error:', error);
+        }
+    }
 
     return (
         <div className={`${open ? 'w-1/3 sm:w-150' : 'w-10'} bg-white border-r border-gray-300 duration-700`}>
@@ -34,7 +63,7 @@ const Sidebar = ({ open, toggleSidebar, companyData, sessionData }) => {
                                 Reload Interview
                             </button>
                             <button
-                                className='sm:ml-2 mb-2 sm:mb-0 text-white bg-indigo-500 hover:bg-indigo-600 transition duration-300 p-1 rounded-r-xl sm:rounded-t-xl text-xs sm:text-base'
+                                className='sm:ml-2 text-white bg-indigo-500 hover:bg-indigo-600 transition duration-300 p-1 rounded-r-xl sm:rounded-t-xl text-xs sm:text-base'
                                 type='submit'
                                 onClick={() => {
                                     navigate("/");
@@ -42,10 +71,19 @@ const Sidebar = ({ open, toggleSidebar, companyData, sessionData }) => {
                             >
                                 Start New Interview
                             </button>
-                            
+                            <button
+                                className='sm:ml-2 mb-2 sm:mb-0 text-white bg-indigo-500 hover:bg-indigo-600 transition duration-300 p-1 rounded-r-xl sm:rounded-t-xl text-xs sm:text-base'
+                                type='submit'
+                                onClick={() => {
+                                    clearSessions()
+                                }}
+                            >
+                                Clear Other Sessions
+                            </button>
+
                             <div className='mr-2 p-2 bg-gray-100 border border-gray-300 mb-2 rounded-r-3xl max-h-48 overflow-scroll scrollbar-hide'>
                                 {sessionData.map((item, index) => (
-                                    <div key={index} className={`inline-grid bg-gray-500 text-white p-1 rounded m-1 ${index === sessionData.length - 1 ? 'bg-indigo-700' : ''}`}>
+                                    <div key={index} className={`inline-grid bg-gray-500 text-white p-1 rounded text-xs sm:text-base m-1 ${index === sessionData.length - 1 ? 'bg-indigo-700' : ''}`}>
                                         <p>{item.company_name}</p>
                                         <p className='text-gray-100 text-sm'>{item.position}</p>
                                         <p className='text-gray-200 text-xs'>{item.languages.join(', ')}</p>
@@ -59,13 +97,13 @@ const Sidebar = ({ open, toggleSidebar, companyData, sessionData }) => {
                             </li>
                         </ul>
                         <div className='max-h-96 overflow-scroll scrollbar-hide pb-2'>
-                            
+
                             <ul className={`bg-gray-50 border border-gray-300 rounded-r-3xl p-2 mr-2 ${open ? '' : 'hidden'} duration-100 text-sm sm:text-base break-words`}>
                                 <li><strong><FaUserAstronaut /></strong> {companyData.question}</li>
                             </ul>
                         </div>
 
-                        
+
                     </div>
                 )}
             </div>
