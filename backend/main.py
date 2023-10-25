d2029ad11d9d29b6f6dc59aeae98c635740d47d6
danmxli <danmuxingli@gmail.com>
2023-10-25 12:16:39 -0400

    Dockerfile

--- backend/main.py
+++ backend/main.py
@@ -78,4 +78,4 @@ def clear_collections(user: User):
     
 
 if __name__ == "__main__":
-  uvicorn.run("main:app", host="0.0.0.0", port=8001, reload=True)
+  uvicorn.run("main:app", host="0.0.0.0", port=os.getenv("PORT", default=8001), reload=True)
