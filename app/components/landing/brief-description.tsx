b78e5d81cf462b44e297cab1f53bbebcea5de401
danmxli <danmuxingli@gmail.com>
2023-12-09 18:17:44 -0500

    landing page rebuild in progress

--- app/components/landing/brief-description.tsx
+++ app/components/landing/brief-description.tsx
@@ -1,42 +1,28 @@
+'use client'
 import Image from 'next/image'
 import conduify from '../../public/conduify.svg'
 import { useRouter } from 'next/navigation';
 
 const BriefDescription = () => {
     const router = useRouter()
-    
+
     return (
-        <>
-            <div className="w-1/2">
-                <h2 className="flex items-center font-semibold sm:text-xl">
-                    Forging the Conduit to Success.
-                </h2>
-                <p className="text-xs sm:text-sm leading-[20px] font-normal">
-                    Aspiring engineer or computer scientist? Our mission is to prepare you for the next big step of your career path.
+        <div className='p-3 flex flex-col'>
+            <div className='flex justify-center'>
+                <Image src={conduify} alt='conduify' priority className='w-72' />
+            </div>
+
+            <div className='p-3 bg-gray-50 rounded shadow max-w-xl'>
+                <h1 className='text-xl'>
+                    AI powered interview guidance and evaluation platform.
+                </h1>
+                <p className='text-gray-600'>
+                    👨🏻‍🏫 your personal interview preparation assistant.
+
+                    💫 Ace your interviews with Conduify. With test-driven methodologies, you will be way ahead of the competition!
                 </p>
             </div>
-            <div className="w-1/2 ml-3 sm:ml-0">
-                <h2 className="flex items-center font-semibold sm:text-xl">
-                    Coherent and Clear.
-                </h2>
-                <p className="text-xs sm:text-sm leading-[20px] font-normal">
-                    Our powerful assistant is driven by <a className='text-indigo-500' href='https://cohere.com/'>artificial intelligence.</a> Accelerate your productiveness with cutting edge technologies.
-                </p>
-            </div>
-            <button onClick={() => {
-                router.push('/about')
-            }}>
-                <div className="ml-3 sm:ml-0 bg-gray-100 hover:bg-indigo-50 shadow-md p-1 rounded-xl">
-                    <Image src={conduify} alt='conduify' width={35} height={35} />
-                    <h2 className="text-sm sm:text-base">
-                        About Conduify.
-                    </h2>
-                    <p className='text-xs text-indigo-500'>
-                        <code>Built with passion, available for all.</code>
-                    </p>
-                </div>
-            </button>
-        </>
+        </div>
     )
 }
 
--- app/components/landing/user-portal.tsx
+++ app/components/landing/user-portal.tsx
@@ -1,3 +1,4 @@
+'use client'
 import { useRouter } from 'next/navigation';
 import { useUser } from '@auth0/nextjs-auth0/client';
 import Image from 'next/image';
@@ -7,11 +8,15 @@ const UserPortal = () => {
     const { user, isLoading } = useUser()
 
     return (
-        <>
+        <div className='p-3'>
+            <h1 className='text-2xl sm:text-7xl text-center mb-9'>Introducing <br/>
+                <span className="text-indigo-800">Conduify.</span>
+            </h1>
             {user ? (
-                <>
+                <div className='grid sm:grid-cols-3 gap-3'>
+                    
                     <button
-                        className='rounded-full w-56 py-4 font-semibold flex items-center justify-center bg-gray-900 text-white hover:cursor-pointer flex gap-x-2'
+                        className='sm:col-span-2 rounded p-3 flex items-center justify-center bg-indigo-600 shadow-inner shadow-indigo-400 hover:bg-indigo-800 text-white'
                         onClick={() => {
                             router.push('/dashboard')
                         }}
@@ -38,35 +43,32 @@ const UserPortal = () => {
                         </div>
                     </button>
                     <a
-                        className='rounded-full w-56 py-4 font-semibold flex items-center justify-center bg-gray-400 text-white hover:cursor-pointer flex gap-x-2'
+                        className='rounded p-3 flex items-center justify-center bg-gray-400 hover:bg-gray-500/75 shadow text-white'
                         href="/api/auth/logout"
                     >
                         <div className='flex items-center gap-3'>
                             Logout
                         </div>
                     </a>
-                </>
+                </div>
 
             ) : (
-                <>
+                <div className='flex justify-center'>
                     {isLoading ? (
-                        <div className='rounded-full w-56 py-4 font-semibold flex items-center justify-center bg-gray-900 text-white hover:cursor-pointer flex gap-x-2'>
+                        <div className='rounded p-3 pl-9 pr-9 inline-flex bg-gray-900 text-white'>
                             Loading details...
                         </div>
                     ) : (
                         <a
-                            className='rounded-full w-56 py-4 font-semibold flex items-center justify-center bg-gray-900 text-white hover:cursor-pointer flex gap-x-2'
+                            className='rounded p-3 pl-20 pr-20 inline-flex bg-indigo-600 shadow-inner shadow-indigo-400 hover:bg-indigo-800 text-white'
                             href='/api/auth/login'
                         >
                             Login
                         </a>
                     )}
-                </>
-
+                </div>
             )}
-
-
-        </>
+        </div>
     )
 }
 
