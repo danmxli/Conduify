5d7d3a666a3d2ce18fc0562f2cd8dad7f3aa9802
danmxli <danmuxingli@gmail.com>
2023-12-13 11:25:46 -0500

    remove async headers

--- app/next.config.js
+++ app/next.config.js
@@ -1,7 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-    reactStrictMode: true,
-
     images: {
         remotePatterns: [
             {
@@ -18,24 +16,6 @@ const nextConfig = {
             },
         ],
     },
+}
 
-    async headers() {
-        return [
-            {
-                source: "/(.*)",
-                headers: [
-                    {
-                        key: "Cross-Origin-Embedder-Policy",
-                        value: "require-corp",
-                    },
-                    {
-                        key: "Cross-Origin-Opener-Policy",
-                        value: "same-origin",
-                    },
-                ],
-            },
-        ];
-    },
-};
-
-module.exports = nextConfig;
+module.exports = nextConfig
