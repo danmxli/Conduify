027a032568efcb57eae6f422cc3a1db9d9b0d22a
danmxli <danmuxingli@gmail.com>
2023-12-27 12:53:03 -0500

    test

--- app/app/user/page.tsx
+++ app/app/user/page.tsx
@@ -1,8 +1,13 @@
 'use client'
 import { withPageAuthRequired } from '@auth0/nextjs-auth0/client'
+import React, { useState, useEffect, useRef } from 'react';
+import Loading from '@/components/shared/loading';
 import Sidebar from '@/components/shared/info-sidebar'
 
 export default withPageAuthRequired(function User({ user }) {
+    const [loading, setLoading] = useState(false)
+    const fetchExecuted = useRef(false)
+
     return (
         <div className='flex'>
             <Sidebar name={user.name} email={user.email} picture={user.picture} />
